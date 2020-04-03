813c41751edbd7801263bf50f9fcf6a59ff3c57e
Saunved <saunved@gmail.com>
2020-04-03 16:19:24 +0530

    Build file fixed

--- gulpfile.js
+++ gulpfile.js
@@ -7,42 +7,66 @@ const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
 
 /* Used to generate correct build files */
-const themes = ['mint','antique', 'amber', 'metal', 'mint-dark', 'mint-light', 'royal']
-const flavors = ['default']
+const themes = ['mint', 'antique', 'amber', 'metal', 'mint-dark', 'mint-light', 'royal'];
+const flavors = ['default'];
 
+gulp.task('demo', async () => {
+/*
+* This gulp task creates the following files:
+* demo/flavor-name/assets/css/theme-name.css
+* for each flavor-theme combination
+*/
 
-gulp.task('sass', async function(){
+	flavors.forEach(flavor => {
+		themes.forEach(theme => {
+			gulp.src([
+				'src/main.scss'
+			])
+				.pipe(inject(gulp.src(`src/themes/${theme}.scss`, {read: false, cwd: './'}), {
+					starttag: '// inject:{{ext}}',
+					endtag: '// endinject',
+					transform(filepath) {
+						return `@import "${filepath}";\n@import "./src/flavors/${flavor}/${flavor}.scss";`;
+					}
+				}))
+				.pipe(sass().on('error', sass.logError))
+				.pipe(rename(`${theme}.css`))
+				.pipe(gulp.dest(`./demo/${flavor}/assets/css`));
+		});
+	});
+});
 
-    /*
-     * This gulp task creates the following files: 
-     build/flavor-name/theme-name.css 
-     build/flavor-name/theme-name.min.css
-     demo/flavor-name/assets/css/theme-name.css
-     for each flavor-theme combination
-     */
-    
-    flavors.forEach(flavor => {
-        themes.forEach(theme => {
-            gulp.src([
-                `src/main.scss`
-            ])
-            .pipe(inject(gulp.src(`src/themes/${theme}.scss`, {read: false, cwd: './'}), {
-                starttag: '// inject:{{ext}}',
-                endtag: '// endinject',
-                transform: function(filepath){
-                    return `@import "${filepath}";\n@import "./src/flavors/${flavor}/${flavor}.scss";`;
-                }
-            }))
-            .pipe(sass().on('error', sass.logError))
-            .pipe(rename(`${theme}.css`))
-            .pipe(gulp.dest(`./demo/${flavor}/assets/css`))
-            .pipe(gulp.dest(`./build/${flavor}`))
-            .pipe(cleanCSS())
-            .pipe(rename(`${theme}.min.css`))
-            .pipe(gulp.dest(`./build/${flavor}`))
-        })
-    })
+gulp.task('build', async () => {
+/*
+    * This gulp task creates the following files:
+    build/flavor-name/theme-name.css
+    build/flavor-name/theme-name.min.css
+    for each flavor-theme combination
+*/
 
-})
+	flavors.forEach(flavor => {
+		themes.forEach(theme => {
+			gulp.src([`./src/flavors/${flavor}/${flavor}.scss`])
+				.pipe(inject(gulp.src(`src/themes/${theme}.scss`, {read: false, cwd: './'}), {
+					starttag: '// inject:{{ext}}',
+					endtag: '// endinject',
+					transform(filepath) {
+						return `@import "${filepath}";`;
+					}
+				}))
+				.pipe(sass().on('error', sass.logError))
+				.pipe(rename(`${theme}.css`))
+				.pipe(gulp.dest(`./build/${flavor}`))
+				.pipe(cleanCSS())
+				.pipe(rename(`${theme}.min.css`))
+				.pipe(gulp.dest(`./build/${flavor}`));
+		});
+	});
+});
 
-gulp.watch(['./**/*.scss', './**./*.scss'], gulp.series(['sass']))
+gulp.task('run', async () => {
+	gulp.series(gulp.task('demo'), gulp.task('build'));
+	// Gulp.task('demo')();
+	// gulp.task('build')();
+	gulp.watch(['./**/*.scss', './**./*.scss'], gulp.series(['demo', 'build']));
+});
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -1,62 +1,60 @@
 'use strict';
 class Sidenav {
-    
-    openTrigger;
-    closeTrigger;
-    sidenav;
-    id;
+	// OpenTrigger;
+	// closeTrigger;
+	// sidenav;
+	// id;
 
-    constructor(id) {
-        this._setupSelectors(id);
-        this._setupClickListeners();
-        Sidenav._bindEsc();
-    }
+	constructor(id) {
+		this._setupSelectors(id);
+		this._setupClickListeners();
+		Sidenav._bindEsc();
+	}
 
-    _setupSelectors(id) {
-        this.id = id;
-        this.openTrigger = document.querySelector(`#${id} .trigger`);
-        this.sidenav = document.querySelector(`#${id} .sidenav`);
-        this.closeTrigger = document.querySelector(`#${id} .close-trigger`);
-    }
+	_setupSelectors(id) {
+		this.id = id;
+		this.openTrigger = document.querySelector(`#${id} .trigger`);
+		this.sidenav = document.querySelector(`#${id} .sidenav`);
+		this.closeTrigger = document.querySelector(`#${id} .close-trigger`);
+	}
 
-    _setupClickListeners() {
-        if (this.openTrigger) {
-            this.openTrigger.addEventListener('click', () => {
-                this.sidenav.classList.add('active');
-            })
-        }
+	_setupClickListeners() {
+		if (this.openTrigger) {
+			this.openTrigger.addEventListener('click', () => {
+				this.sidenav.classList.add('active');
+			});
+		}
 
-        if (this.closeTrigger) {
-            this.closeTrigger.addEventListener('click', () => {
-                this.sidenav.classList.remove('active');
-            })
-        }
-    }
+		if (this.closeTrigger) {
+			this.closeTrigger.addEventListener('click', () => {
+				this.sidenav.classList.remove('active');
+			});
+		}
+	}
 
-    static _bindEsc() {
-        document.onkeyup = (e) => {
-            if (e.which == 27) {
-                let sidenavs = document.querySelectorAll('.sidenav');
-                sidenavs.forEach(sidenav => {
-                    sidenav.classList.remove('active');
-                })
-            }
-        }
-    }
+	static _bindEsc() {
+		document.addEventListener('keyup', event => {
+			if (event.key === 'Escape') {
+				const sidenavs = document.querySelectorAll('.sidenav');
+				sidenavs.forEach(sidenav => {
+					sidenav.classList.remove('active');
+				});
+			}
+		});
+	}
 
-    close(){
-        this.sidenav.classList.remove('active');
-    }
+	close() {
+		this.sidenav.classList.remove('active');
+	}
 
-    open(){
-        this.sidenav.classList.add('active');
-    }
-
+	open() {
+		this.sidenav.classList.add('active');
+	}
 }
 
-class Overlay extends Sidenav{
-    constructor(id){
-        super(id);
-        this.sidenav.classList.add('plain');
-    }
+class Overlay extends Sidenav {
+	constructor(id) {
+		super(id);
+		this.sidenav.classList.add('plain');
+	}
 }
